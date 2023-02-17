eb9f222bdda5f69f328c5c6d83cad1da46a60f12
krishna <brkkrishna9@gmail.com>
2023-02-17 20:39:32 +0530

    new changes

--- main.tf
+++ main.tf
@@ -17,29 +17,29 @@ resource "aws_vpc_peering_connection" "peer" {
   )
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-  tags = merge(
-    local.common_tags,
-    { Name = "${var.env}-igw" }
-  )
-}
-
-resource "aws_eip" "ngw-eip" {
-  vpc = true
-}
-
-resource "aws_nat_gateway" "ngw" {
-  allocation_id = aws_eip.ngw-eip.id
-  subnet_id     = var.public_subnet_ids[0]
-
-  tags = merge(
-    local.common_tags,
-    { Name = "${var.env}-ngw" }
-  )
-
-  //depends_on = [aws_internet_gateway.example]
-}
+#resource "aws_internet_gateway" "igw" {
+#  vpc_id = aws_vpc.main.id
+#  tags = merge(
+#    local.common_tags,
+#    { Name = "${var.env}-igw" }
+#  )
+#}
+#
+#resource "aws_eip" "ngw-eip" {
+#  vpc = true
+#}
+#
+#resource "aws_nat_gateway" "ngw" {
+#  allocation_id = aws_eip.ngw-eip.id
+#  subnet_id     = var.subnets
+#
+#  tags = merge(
+#    local.common_tags,
+#    { Name = "${var.env}-ngw" }
+#  )
+#
+#  //depends_on = [aws_internet_gateway.example]
+#}
 
 resource "aws_route" "r" {
   route_table_id            = data.aws_vpc.default.main_route_table_id
